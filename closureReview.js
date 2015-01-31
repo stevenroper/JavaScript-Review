f2f27f7c9f5d5694a54e906a46d615f709ffa87b
Steven Roper <steven.m.roper@gmail.com>
2015-01-31 12:24:24 -0700

    Finishing 2nd problem and this project.

--- closureReview.js
+++ closureReview.js
@@ -15,9 +15,34 @@ console.log(inner('435-215-9248'));
 
 
 /*
-
 Write a function that accepts a function as it's first argument and returns a new function (which calls the original function that was passed in) that can only ever be executed once.
 
 Once completed, add a second arguments that allows the function to be executed N number of times. After the function has been called N number of times, console.log('STAHHP');
-
 */
+
+
+var firstFn = function (iterator, cb) {
+    var count = 1;
+    return function () {
+        if (count <= iterator) {
+            cb();
+            count++;
+        } else {
+            console.log('STAHHP!');
+        }
+    }
+};
+
+var secondFn = function () {
+    console.log('Sup.');
+};
+
+var innerFn = firstFn(3, secondFn);
+innerFn();
+innerFn();
+innerFn();
+innerFn();
+innerFn();
+innerFn();
+innerFn();
+innerFn();
