240e593082cefc903ec150a9178c1154db3a41a0
Steven Roper <steven.m.roper@gmail.com>
2015-01-31 11:23:13 -0700

    Finishing 2nd problem.

--- objectReview.js
+++ objectReview.js
@@ -41,15 +41,21 @@ var user = {
 
 //Above you're given a user object. Loop through the user object checking to make sure that each value is truthy. If it's not truthy, remove it from the object. hint: 'delete'.
 
-  //Your code here
+for (var key in user) {
+    if(!user[key]) {
+        delete user[key];
+    }
+}
 
 //Once you get your truthy object, Change the remaining values in the object to be specific to you (name: 'your name', username: 'your username'), rather than my information.
 
-  //Your code here
+user.name = 'Steven';
+user.username = 'stevenbub';
+user.pwHash = '193fja;xkd9120fjaks';
 
 //Now console.log your object and make sure it looks right.
 
-  //Your code here
+console.log(user);
 
 
 
